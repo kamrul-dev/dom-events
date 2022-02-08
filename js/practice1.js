15a97a6511c5d0da2ec1107743edd2304f94fa34
Kamrul Hasan <you@example.com>
2022-02-08 14:52:23 +0600

    way 3: to add onclick event handler using function name and id selector

--- js/practice1.js
+++ js/practice1.js
@@ -1,3 +1,11 @@
+//way 2: to add onclick event handler using onclick function call
 function makeBlueButton(){
     document.body.style.backgroundColor = 'blue';
+}
+
+// way 3: to add onclick event handler using function name and id selector
+const Indigobutton = document.getElementById('make-indigo');
+Indigobutton.onclick = makeIndioColor;
+function makeIndioColor(){
+    document.body.style.backgroundColor = 'indigo';
 }
